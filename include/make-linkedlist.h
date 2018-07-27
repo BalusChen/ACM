c644836b45af78fbdc8c0c91c6e2ccdda7c4f237
lelouch <lelouch.279978955@gmail.com>
2018-07-27 15:13:42 +0800

    problem-147 finished! And finished the header of making a linkedlist

--- include/make-linkedlist.h
+++ include/make-linkedlist.h
@@ -1,16 +1,25 @@
 #ifndef MAKE_LINKEDLIST_H__
 #define MAKE_LINKEDLIST_H__
 
+#include "listnode.h"
 #include <random>
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x): val(x), next(nullptr) {  }
-};
+#define MIN_ELEM 3
+#define MAX_ELEM 17
 
 ListNode* MakeLinkedList() {
+    static std::uniform_int_distribution<int> u(MIN_ELEM, MAX_ELEM
+            );
+    static std::default_random_engine e;
+    int sz = u(e);
     ListNode pre_head(0);
+
+    ListNode *ptr = &pre_head;
+    while (--sz >= 0) {
+        ptr->next = new ListNode(u(e));
+        ptr = ptr->next;
+    }
+
     return pre_head.next;
 }
 
