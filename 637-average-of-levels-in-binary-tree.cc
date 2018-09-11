b99ae0f2e927e5d7de0105028f706a65785a5423
lelouch <lelouch.279978955@gmail.com>
2018-09-11 15:02:16 +0800

    637: averages of levels in binary tree

---
+++ 637-average-of-levels-in-binary-tree.cc
@@ -0,0 +1,43 @@
+#include <vector>
+#include <queue>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int _val) :
+        val(_val), left(nullptr), right(nullptr) {  }
+};
+
+class Solution {
+    public:
+        std::vector<double> averageOfLevels(TreeNode *root) {
+            if (root == nullptr)
+                return averages;
+
+            std::queue<TreeNode*> q;
+            TreeNode *top;
+            double sum;
+            int sz;
+
+            q.push(root);
+            while (!q.empty()) {
+                sum = 0;
+                sz = q.size();
+                for (int i = 0; i < sz; ++i) {
+                    top = q.front();
+                    q.pop();
+                    sum += top->val;
+                    if (top->left != nullptr)
+                        q.push(top->left);
+                    if (top->right != nullptr)
+                        q.push(top->right);
+                }
+                averages.push_back(sum / sz);
+            }
+
+            return averages;
+        }
+
+        std::vector<double> averages;
+};
