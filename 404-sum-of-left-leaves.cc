8d28f6d710c2c74f8c2cb6f2920eb5249e6160cb
lelouch <lelouch.279978955@gmail.com>
2018-09-21 21:46:04 +0800

    404: sum of left leaves

---
+++ 404-sum-of-left-leaves.cc
@@ -0,0 +1,22 @@
+#include "include/make-tree.h"
+
+class Solution {
+    public:
+        int sumOfLeftLeaves(TreeNode *root) {
+            int sum = 0;
+            if (root == nullptr)
+                return 0;
+            if (root->left != nullptr && IsLeaf(root->left)) {
+                sum += root->left->val;
+            } else {
+                sum += sumOfLeftLeaves(root->left);
+            }
+            if (root->right != nullptr)
+                sum += sumOfLeftLeaves(root->right);
+            return sum;
+        }
+
+        bool IsLeaf(TreeNode *root) {
+            return (root->left == nullptr) && (root->right == nullptr);
+        }
+};
