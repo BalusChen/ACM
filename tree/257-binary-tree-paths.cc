a99200ed5a204a45847357b824a8c97dc3381fcc
lelouch <lelouch.279978955@gmail.com>
2018-09-25 15:35:41 +0800

    make member functiom public

--- tree/257-binary-tree-paths.cc
+++ tree/257-binary-tree-paths.cc
@@ -2,22 +2,23 @@
 #include <string>
 
 class Solution {
-    std::vector<std::string> binaryTreePaths(TreeNode *root) {
-        if (root == nullptr)
-            return std::vector<std::string>();
+    public:
+        std::vector<std::string> binaryTreePaths(TreeNode *root) {
+            if (root == nullptr)
+                return std::vector<std::string>();
 
-        auto left = binaryTreePaths(root->left);
-        auto right = binaryTreePaths(root->right);
-        std::vector<std::string> ret;
-        if (left.empty() && right.empty()) {
-            ret.push_back(std::to_string(root->val));
-        } else {
-            for (const auto &c : left)
-                ret.push_back(std::to_string(root->val) + "->" + c);
-            for (const auto &c : right)
-                ret.push_back(std::to_string(root->val) + "->" + c);
+            auto left = binaryTreePaths(root->left);
+            auto right = binaryTreePaths(root->right);
+            std::vector<std::string> ret;
+            if (left.empty() && right.empty()) {
+                ret.push_back(std::to_string(root->val));
+            } else {
+                for (const auto &c : left)
+                    ret.push_back(std::to_string(root->val) + "->" + c);
+                for (const auto &c : right)
+                    ret.push_back(std::to_string(root->val) + "->" + c);
+            }
+
+            return ret;
         }
-
-        return ret;
-    }
 };
