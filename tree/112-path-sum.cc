ba3d23da0a0ce0c9114f50b05d55d9885c3bd546
lelouch <lelouch.279978955@gmail.com>
2018-09-30 21:49:55 +0800

    112: path sum. runtime beats 100.00% of cpp submissions

---
+++ tree/112-path-sum.cc
@@ -0,0 +1,24 @@
+#include "../include/make-tree.h"
+
+class Solution {
+    public:
+        bool hasPathSum(TreeNode *root, int sum) {
+            if (root == nullptr)
+                return false;
+            if (!IsLeaf(root)) {
+                sum -= root->val;
+                return hasPathSum(root->left, sum) ||
+                       hasPathSum(root->right, sum);
+            } else {
+                if ((sum -= root->val) == 0)
+                    return true;
+                else
+                    return false;
+            }
+        }
+
+        bool IsLeaf(TreeNode *n) {
+            return n->left == nullptr && n->right == nullptr; 
+        }
+
+};
