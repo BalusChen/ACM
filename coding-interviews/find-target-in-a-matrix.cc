5d701db4460fceb479a99a11fb52f2fb5ee669d1
tangmou <balus@foxmail.com>
2019-04-08 15:42:56 +0800

    find target in a matrix

---
+++ coding-interviews/find-target-in-a-matrix.cc
@@ -0,0 +1,34 @@
+#include <vector>
+#include <stdio.h>
+#include <iostream>
+
+class Solution {
+public:
+    bool Find(int target, std::vector<std::vector<int>> array)
+    {
+        int lo, hi, mid;
+        int val;
+
+        for (int i = 0; i < array.size(); i++) {
+            lo = 0;
+            hi = array[i].size() - 1;
+
+            while (lo <= hi) {
+                mid = lo + (hi - lo) / 2;
+                val = array[i][mid];
+
+                if (val < target) {
+                    lo = mid + 1;
+                } else if (val > target) {
+                    hi = mid - 1;
+                } else {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+};
+
