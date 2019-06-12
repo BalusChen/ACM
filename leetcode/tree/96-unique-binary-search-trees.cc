5560df5bda4ea054b28af09c60fc0b8b5b7e3bb9
balus <balus@foxmail.com>
2019-06-12 16:31:02 +0800

    make program prettier
        1. change function name
        2. `n` should be specified in command line
        3. display the `dp` array

--- leetcode/tree/96-unique-binary-search-trees.cc
+++ leetcode/tree/96-unique-binary-search-trees.cc
@@ -1,33 +1,44 @@
-#include <stdio.h>
+
+/*
+ * Copyright (C) Jianyong Chen
+ */
+
+#include <cstdio>
 #include <vector>
-#include <stdlib.h>
+#include <cstdlib>
+
 
 std::vector<std::vector<int>> dp;
 
-int aux_1(int lo, int root, int hi);
-int aux_2(int lo, int hi);
+
+static int aux_1(int lo, int hi);
+static int aux_2(int lo, int root, int hi);
+
 
 int numTrees(int n) {
     dp.resize(n);
+
     for (int i = 0; i < n; i++) {
         dp[i] = std::vector<int>(n, 0);
     }
-    return aux_2(1, n);
+
+    return aux_1(1, n);
 }
 
 
-int aux_1(int lo, int root, int hi) {
-    int     num_left, num_right, sum;
+int aux_2(int lo, int root, int hi) {
+    int  num_left, num_right, sum;
 
     if (lo >= hi) {
         return 1;
     }
 
-    num_left = aux_2(lo, root-1);
-    num_right = aux_2(root+1, hi);
+    num_left = aux_1(lo, root-1);
+    num_right = aux_1(root+1, hi);
 
     if (num_left == 0 || num_right == 0) {
         sum = (num_left == 0) ? num_right : num_left;
+
     } else {
         sum = num_left * num_right;
     }
@@ -35,20 +46,21 @@ int aux_1(int lo, int root, int hi) {
     return sum;
 }
 
-int aux_2(int lo, int hi) {
+int aux_1(int lo, int hi) {
     int     sum;
 
     if (lo > hi) {
         return 0;
     }
 
+    // 由于 lo 和 hi 是从 1 开始的，所以从 dp 中取数据时要-1
     if (dp[lo-1][hi-1] != 0) {
         return dp[lo-1][hi-1];
     }
 
     sum = 0;
     for (int i = lo; i <= hi; i++) {
-        sum += aux_1(lo, i, hi);
+        sum += aux_2(lo, i, hi);
     }
 
     dp[lo-1][hi-1] = sum;
@@ -58,11 +70,26 @@ int aux_2(int lo, int hi) {
 
 
 int
-main(int arg, char **argv)
+main(int argc, char **argv)
 {
     int     n, sum;
 
-    n = 18;
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s n\n", argv[0]);
+        exit(-1);
+    }
+
+    n = atoi(argv[1]);
     sum = numTrees(n);
     printf("num = %d\n", sum);
+
+    for (auto v : dp) {
+        for (auto c : v) {
+            printf("%d  ", c);
+        }
+
+        printf("\n");
+    }
+
+    exit(0);
 }
