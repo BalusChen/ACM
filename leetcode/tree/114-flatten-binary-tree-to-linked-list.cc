c5c98f6c99c807551595aaf189fea4fa8bac3714
balus <balus@foxmail.com>
2019-07-07 11:42:30 +0800

    114: flatten binary tree to linked list (in place)
    Difficulty: medium
    Runtime: 8 ms, faster than 72.46% of C++ online submissions for Flatten Binary Tree to Linked List.
    Memory Usage: 9.6 MB, less than 72.58% of C++ online submissions for
    Flatten Binary Tree to Linked List.

---
+++ leetcode/tree/114-flatten-binary-tree-to-linked-list.cc
@@ -0,0 +1,84 @@
+
+/*
+ * Copyright (C) Jianyong Chen
+ */
+
+
+#include "../include/make-tree.h"
+#include <stack>
+
+class Solution
+{
+    public:
+        void flatten(TreeNode *root) {
+            aux(root);
+        }
+
+        TreeNode *aux(TreeNode *root) {
+            TreeNode  *right, *p;
+
+            if (root == nullptr) {
+                return root;
+            }
+
+            right = root->right;
+            root->right = aux(root->left);
+            root->left = nullptr;
+
+            p = root;
+            while (p->right != nullptr) {
+                p = p->right;
+            }
+
+            p->right = aux(right);
+
+            return root;
+        }
+};
+
+
+int
+main(int argc, char **argv)
+{
+    Solution           ans;
+    TreeNode           dummy_head(0), *root;
+    std::vector<int>   elts;
+
+    elts = { 1, 2, 5, 3, 4, INT_MAX, 6 };
+    root = MakeTree(elts);
+    ans.flatten(root);
+    PrintTree(root);
+
+    printf("\n");
+    root = &dummy_head;
+    root->right = new TreeNode(0); root = root->right;
+    root->right = new TreeNode(1); root = root->right;
+    root->right = new TreeNode(2); root = root->right;
+    root->right = new TreeNode(3); root = root->right;
+    root->right = new TreeNode(4);
+    ans.flatten(dummy_head.right);
+    PrintTree(dummy_head.right);
+    delete dummy_head.right;
+
+    printf("\n");
+    root = &dummy_head;
+    root->left = new TreeNode(7); root = root->left;
+    root->left = new TreeNode(6); root = root->left;
+    root->left = new TreeNode(5); root = root->left;
+    root->left = new TreeNode(4); root = root->left;
+    root->left = new TreeNode(3); root = root->left;
+    root->left = new TreeNode(2); root = root->left;
+    root->left = new TreeNode(1); root = root->left;
+    root->left = new TreeNode(0);
+    ans.flatten(dummy_head.right);
+    PrintTree(dummy_head.right);
+    delete dummy_head.left;
+
+    printf("\n");
+    elts = { 1, INT_MAX, 2, 3 };
+    root = MakeTree(elts);
+    ans.flatten(root);
+    PrintTree(root);
+
+    return 0;
+}
