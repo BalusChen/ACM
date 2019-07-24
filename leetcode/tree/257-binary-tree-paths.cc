4d3437d91ae0058a3f8d1d4fd3631140963557b2
balus <balus@foxmail.com>
2019-07-24 10:34:13 +0800

    beautify 257...

--- leetcode/tree/257-binary-tree-paths.cc
+++ leetcode/tree/257-binary-tree-paths.cc
@@ -1,22 +1,32 @@
+
+/*
+ * Copyright (C) Jianyong Chen
+ */
+
 #include "../include/make-tree.h"
 #include <string>
 
 class Solution {
     public:
         std::vector<std::string> binaryTreePaths(TreeNode *root) {
-            if (root == nullptr)
+            std::vector<std::string>  ret;
+
+            if (root == nullptr) {
                 return std::vector<std::string>();
+            }
 
             auto left = binaryTreePaths(root->left);
             auto right = binaryTreePaths(root->right);
-            std::vector<std::string> ret;
             if (left.empty() && right.empty()) {
                 ret.push_back(std::to_string(root->val));
+
             } else {
-                for (const auto &c : left)
+                for (const auto &c : left) {
                     ret.push_back(std::to_string(root->val) + "->" + c);
-                for (const auto &c : right)
+                }
+                for (const auto &c : right) {
                     ret.push_back(std::to_string(root->val) + "->" + c);
+                }
             }
 
             return ret;
