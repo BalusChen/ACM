8f5175143afcb552bf5779d9fd27c9c1ca2d8332
balus <balus@foxmail.com>
2019-08-10 08:27:29 +0800

    Nowcoder: 3 print linked list from tail

---
+++ nowcoder/3-print-linked-list-from-tail.cc
@@ -0,0 +1,32 @@
+
+/*
+ * Copyright (C) Jianyong Chen
+ */
+
+
+#include "../leetcode//include/make-linkedlist.h"
+
+
+class Solution {
+    public:
+        std::vector<int> printListFromTailToHead(ListNode* head) {
+            aux(head);
+
+            return sequence;
+        }
+
+        void aux(ListNode *head) {
+            if (head == nullptr) {
+                return;
+            }
+
+            if (head->next == nullptr) {
+                sequence.push_back(head->val);
+            } else {
+                aux(head->next);
+                sequence.push_back(head->val);
+            }
+        }
+
+        std::vector<int>  sequence;
+};
