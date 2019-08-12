44ee659d439b7cc05c66dc237de7b8421d44c484
balus <balus@foxmail.com>
2019-08-12 10:52:16 +0800

    use std::swap for simplicity

--- nowcoder/18-binary-tree-mirror.cc
+++ nowcoder/18-binary-tree-mirror.cc
@@ -10,16 +10,11 @@
 class Solution {
     public:
         void Mirror(TreeNode *root) {
-            TreeNode  *tmp;
-
             if (root == nullptr) {
                 return;
             }
 
-            tmp = root->left;
-            root->left = root->right;
-            root->right = tmp;
-
+            std::swap(root->left, root->right);
             Mirror(root->left);
             Mirror(root->right);
         }
