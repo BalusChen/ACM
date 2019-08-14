0eb990c5cf1b85da648bab0a964d623ce63a08f1
balus <balus@foxmail.com>
2019-08-14 14:31:35 +0800

    Add another 2 solutions.

--- nowcoder/11-number-of-1.cc
+++ nowcoder/11-number-of-1.cc
@@ -28,11 +28,47 @@ class Solution {
 };
 
 
+class Solution2 {
+    public:
+        int  NumberOf1(int n) {
+            int  count, mask;
+
+            count = 0;
+            mask = 1;
+            while (mask != 0) {
+                if ((mask & n) != 0) {
+                    count++;
+                }
+                mask <<= 1;
+            }
+
+            printf("count: %d\n", count);
+            return count;
+        }
+};
+
+
+class Solution3 {
+    public:
+        int  NumberOf1(int n) {
+            int  count;
+
+            count = 0;
+            while (n != 0) {
+                count++;
+                n &= (n - 1);
+            }
+
+            return count;
+        }
+};
+
+
 int
 main(int argc, char **argv)
 {
-    int       n;
-    Solution  ans;
+    int        n;
+    Solution2  ans;
 
     n = 7;
     ans.NumberOf1(n);
