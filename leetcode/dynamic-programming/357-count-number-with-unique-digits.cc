048d806882b3de75f9e8cbb8396e99cd260e477d
balus <balus@foxmail.com>
2019-08-17 09:42:16 +0800

    357: count number with unique digits
    Difficulty: medium
    Runtime: 4 ms, faster than 51.94% of C++ online submissions for Count Numbers with Unique Digits.
    Memory Usage: 8.5 MB, less than 16.67% of C++ online submissions for Count Numbers with Unique Digits.

---
+++ leetcode/dynamic-programming/357-count-number-with-unique-digits.cc
@@ -0,0 +1,53 @@
+
+/*
+ * Copyright (C) Jianyong Chen
+ */
+
+
+#include <vector>
+#include <cmath>
+#include <numeric>
+
+
+class Solution {
+    public:
+        int countNumbersWithUniqueDigits(int n) {
+            int               i;
+            std::vector<int>  dp;
+
+            if (n < 2) {
+                return std::pow(10, n);
+            }
+
+            dp.resize(n+1);
+
+            /*
+             * if `n` == 0; then 1 is the only only in [0, 2^0],
+             * and it has been calculated in previous `if` statement
+             *
+             * if `n` > 0; then 1 can be in dp[0] and dp[1];
+             * so it will be an error if we do not handle this case.
+             * to avoid calculate twice, we simply set dp[0] = 0;
+             */
+
+            dp[0] = 0;
+            dp[1] = 10;
+            for (i = 2; i <= n; i++) {
+                dp[i] = countUniqueOfLengthK(i);
+            }
+
+            return std::accumulate(dp.begin(), dp.end(), 0);
+        }
+
+        int countUniqueOfLengthK(int k) {
+            int  count, i;
+
+            /* k >= 2 */
+            count = 9;
+            for (i = 2; i <= k; i++) {
+                count *= (9 - i + 2);
+            }
+
+            return count;
+        }
+};
