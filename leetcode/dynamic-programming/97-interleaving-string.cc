839048dccc46ae1849ee07a8af00d1af777f6f04
balus <balus@foxmail.com>
2019-08-30 09:02:40 +0800

    Refactor leetcode-97
    Divide the big for loop into 3 smaller for loops

--- leetcode/dynamic-programming/97-interleaving-string.cc
+++ leetcode/dynamic-programming/97-interleaving-string.cc
@@ -18,23 +18,21 @@ class Solution {
             int   i, j;
             bool  dp[s1.size()+1][s2.size()+1];
 
-            for (i = 0; i <= s1.size(); i++) {
+            dp[0][0] = true;
 
-                for (j = 0; j <= s2.size(); j++) {
+            for (i = 1; i <= s1.size(); i++) {
+                dp[i][0] = dp[i-1][0] && (s1[i-1] == s3[i-1]);
+            }
 
-                    if (i == 0 && j == 0) {
-                        dp[i][j] = true;
+            for (j = 1; j <= s2.size(); j++) {
+                dp[0][j] = dp[0][j-1] && (s2[j-1] == s3[j-1]);
+            }
 
-                    } else if (i == 0) {
-                        dp[i][j] = dp[i][j-1] && (s2[j-1] == s3[i+j-1]);
+            for (i = 1; i <= s1.size(); i++) {
 
-                    } else if (j == 0) {
-                        dp[i][j] = dp[i-1][j] && (s1[i-1] == s3[i+j-1]);
-
-                    } else {
-                        dp[i][j] = (dp[i-1][j] && (s1[i-1] == s3[i+j-1]))
-                                   || (dp[i][j-1] && (s2[j-1] == s3[i+j-1]));
-                    }
+                for (j = 1; j <= s2.size(); j++) {
+                    dp[i][j] = (dp[i-1][j] && (s1[i-1] == s3[i+j-1]))
+                               || (dp[i][j-1] && (s2[j-1] == s3[i+j-1]));
                 }
             }
 
